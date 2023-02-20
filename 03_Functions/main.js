248f745928517b45f8202c472181363d07e271b1
danielOuattara <daniel.ouattara@gmx.com>
2023-02-20 21:03:29 +0100

    update function logic

--- 03_Functions/main.js
+++ 03_Functions/main.js
@@ -1,26 +1,19 @@
+"use strict";
 /* VIDEO TIME: 17:27'  */
-
-const getFullName = (fName, lName) => {
-  return `${fName} ${lName}`;
+var getFullName = function (fName, lName) {
+    return "".concat(fName, " ").concat(lName);
 };
-
 console.log(getFullName("John", "Doe")); // OK
 console.log(getFullName(true, 2)); // Accepted but Non sense
-
 //-------------------------------------------------------
-
-const getFullName2 = (fName: string, lName: string) => {
-  return `${fName} ${lName}`;
+var getFullName2 = function (fName, lName) {
+    return "".concat(fName, " ").concat(lName);
 };
-
 // console.log(getFullName2(true, 2)); // X Incorrect
 console.log(getFullName2("John", "Doe")); // Correct
-
 //-------------------------------------------------------
-
-const getFullName3 = (fName: string, lName: string): string => {
-  return `${fName} ${lName}`;
+var getFullName3 = function (fName, lName) {
+    return "".concat(fName, " ").concat(lName);
 };
-
 // console.log(getFullName3(true, 2)); // X Incorrect
 console.log(getFullName3("John", "Doe")); // Correct
