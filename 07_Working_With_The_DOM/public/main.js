93201249bf0d521098068bb1f34ddf0de651fd68
danielOuattara <daniel.ouattara@gmx.com>
2023-02-20 21:05:10 +0100

    update logic for DOM

--- 07_Working_With_The_DOM/public/main.js
+++ 07_Working_With_The_DOM/public/main.js
@@ -1,24 +1,73 @@
 "use strict";
 //---------------------------------------------
 // Recall Type Assertion / Type Casting
-let page = "1";
-let paegeNumber = page;
+var page = "1";
+var pageNumber = page;
 // ---------------------------------------------
-// Working with the DOM
-const someElement = document.querySelector(".foo");
-// console.log(someElement.value); // What ?  Yes ! cool!  You have to be more specific: try again !
-const someElement_2 = document.querySelector(".foo"); // if it is an input element, for example
+/* Working with the DOM
+-------------------------
+Note:
+ - TypeScript has a lot of types for handling DOM out of the box
+ - Element in TypeScript is the highest class in hierarchy
+ - HTML Event is also in th highest class in hierarchy
+ */
+var someElement = document.querySelector(".foo"); // not specific
+// console.log(someElement.)  // auto completion should be available
+console.log(someElement.value); // Incorrect: You have to be more specific: try again !
+// here the solution: precise the type of element using type assertion.
+var someElement_2 = document.querySelector(".foo"); // if it is an input element, for example
 console.log(someElement_2.value); // Cool !
-// element is the highest class in hierachy
+console.log(someElement_2.focus()); // Cool !
+console.log(someElement_2.blur()); // Cool !
+var someElement_2_bis = document.querySelector(".foo");
+console.log(someElement_2_bis.href); // Cool !
+console.log(someElement_2_bis.innerHTML); // Cool !
+var someElement_2_ter = document.querySelector(".foo"); // if it is an input element, for example
+console.log(someElement_2_ter.style); // Cool !
+console.log(someElement_2_ter.onmouseover); // Cool !
 //-----------------------------------------------
 // Adding a Listener
-const someElement_3 = document.querySelector(".foo");
-someElement_3 === null || someElement_3 === void 0 ? void 0 : someElement_3.addEventListener("blur", (event) => {
-    const target = event.target;
+var someElement_3 = document.querySelector(".foo");
+someElement_3.addEventListener("blur", function (event) {
+    console.log(event.target.value);
+});
+//---
+var someElement_4 = document.querySelector(".foo");
+someElement_4.addEventListener("blur", function (event) {
+    var target = event.target;
     console.log(target.value);
 });
-const someElement_4 = document.querySelector(".foo"); // if it is an input element, for example
-someElement_4.addEventListener("blur", (event) => {
-    const target = event.target;
-    console.log(target.value);
+//---
+var someElement_5 = document.querySelector(".foo");
+someElement_5 === null || someElement_5 === void 0 ? void 0 : someElement_5.addEventListener("blur", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
+//---
+var someElement_6 = document.querySelector(".foo"); // if it is an input element, for example
+someElement_6.addEventListener("blur", function (event) {
+    console.log(event.target.value);
+});
+//---
+var someElement_7 = document.querySelector(".foo"); // if it is an input element, for example
+someElement_7.addEventListener("blur", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
+//---
+var someElement_8 = document.querySelector(".foo"); // if it is an input element, for example
+someElement_8.addEventListener("mouseover", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
+//---
+var someElement_9 = document.querySelector(".foo"); // if it is an input element, for example
+someElement_9.addEventListener("copy", function (event) {
+    var eventTarget = event.target;
+    console.log(eventTarget.value);
+});
+//---
+var someElement_10 = document.querySelector(".foo"); // if it is an input element, for example
+someElement_10.addEventListener("copy", function (event) {
+    console.log(event.target.value);
 });
--- 07_Working_With_The_DOM/src/main.ts
+++ 07_Working_With_The_DOM/src/main.ts
@@ -2,31 +2,97 @@
 // Recall Type Assertion / Type Casting
 
 let page: any = "1";
-let paegeNumber = page as string;
+let pageNumber = page as string;
 
 // ---------------------------------------------
 
-// Working with the DOM
+/* Working with the DOM
+-------------------------
+Note: 
+ - TypeScript has a lot of types for handling DOM out of the box
+ - Element in TypeScript is the highest class in hierarchy   
+ - HTML Event is also in th highest class in hierarchy
+ */
 
-const someElement = document.querySelector(".foo");
-// console.log(someElement.value); // What ?  Yes ! cool!  You have to be more specific: try again !
+const someElement = document.querySelector(".foo"); // not specific
 
+// console.log(someElement.)  // auto completion should be available
+
+console.log(someElement.value); // Incorrect: You have to be more specific: try again !
+
+// here the solution: precise the type of element using type assertion.
 const someElement_2 = document.querySelector(".foo") as HTMLInputElement; // if it is an input element, for example
 console.log(someElement_2.value); // Cool !
-// element is the highest class in hierachy
+console.log(someElement_2.focus()); // Cool !
+console.log(someElement_2.blur()); // Cool !
+
+const someElement_2_bis = document.querySelector(".foo") as HTMLAnchorElement;
+console.log(someElement_2_bis.href); // Cool !
+console.log(someElement_2_bis.innerHTML); // Cool !
+
+const someElement_2_ter = document.querySelector(".foo") as HTMLHtmlElement; // if it is an input element, for example
+console.log(someElement_2_ter.style); // Cool !
+console.log(someElement_2_ter.onmouseover); // Cool !
 
 //-----------------------------------------------
 
 // Adding a Listener
 
 const someElement_3 = document.querySelector(".foo");
-someElement_3?.addEventListener("blur", (event) => {
+someElement_3.addEventListener("blur", (event) => {
+  console.log(event.target.value);
+});
+
+//---
+
+const someElement_4 = document.querySelector(".foo");
+someElement_4.addEventListener("blur", (event) => {
   const target = event.target as HTMLInputElement;
   console.log(target.value);
 });
 
-const someElement_4 = document.querySelector(".foo") as HTMLInputElement; // if it is an input element, for example
-someElement_4.addEventListener("blur", (event) => {
-  const target = event.target as HTMLInputElement;
-  console.log(target.value);
+//---
+
+const someElement_5 = document.querySelector(".foo");
+someElement_5?.addEventListener("blur", (event) => {
+  const eventTarget = event.target as HTMLInputElement;
+  console.log(eventTarget.value);
+});
+
+//---
+
+const someElement_6 = document.querySelector(".foo") as HTMLInputElement; // if it is an input element, for example
+someElement_6.addEventListener("blur", (event) => {
+  console.log(event.target.value);
+});
+
+//---
+
+const someElement_7 = document.querySelector(".foo") as HTMLInputElement; // if it is an input element, for example
+someElement_7.addEventListener("blur", (event) => {
+  const eventTarget = event.target as HTMLInputElement;
+  console.log(eventTarget.value);
+});
+
+//---
+
+const someElement_8 = document.querySelector(".foo") as HTMLInputElement; // if it is an input element, for example
+someElement_8.addEventListener("mouseover", (event) => {
+  const eventTarget = event.target as HTMLInputElement;
+  console.log(eventTarget.value);
+});
+
+//---
+
+const someElement_9 = document.querySelector(".foo") as HTMLInputElement; // if it is an input element, for example
+someElement_9.addEventListener("copy", (event) => {
+  const eventTarget = event.target as HTMLInputElement;
+  console.log(eventTarget.value);
+});
+
+//---
+
+const someElement_10 = document.querySelector(".foo") as HTMLInputElement; // if it is an input element, for example
+someElement_10.addEventListener("copy", (event) => {
+  console.log((event.target as HTMLInputElement).value);
 });
