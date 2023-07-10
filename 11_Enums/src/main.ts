a424e2de6705428c76ea959510dd2e769078e7a5
danielOuattara <daniel.ouattara@gmx.com>
2023-07-10 23:13:33 +0200

    11: enums, logic updated

--- 11_Enums/src/main.ts
+++ 11_Enums/src/main.ts
@@ -6,7 +6,9 @@ const statuses = {
   done: 2,
 };
 
-console.log(statuses.done);
+console.log("statuses.done = ", statuses.done);
+
+//-----------------------------------
 
 enum StatusesEnum {
   NOT_STARTED = "Not started",
@@ -14,10 +16,35 @@ enum StatusesEnum {
   DONE = "DOne",
 }
 
-console.log(StatusesEnum.IN_PROGRESS);
+console.log("StatusesEnum.IN_PROGRESS = ", StatusesEnum.IN_PROGRESS);
 let loadingStatus = StatusesEnum.DONE;
+
+let statusInfo: StatusesEnum = StatusesEnum.NOT_STARTED;
+console.log("statusInfo = ", statusInfo);
+
+// on progress :
+statusInfo = StatusesEnum.IN_PROGRESS;
+console.log("statusInfo = ", statusInfo);
+
+// after completed
+statusInfo = StatusesEnum.DONE;
+console.log("statusInfo = ", statusInfo);
+
+//--------------------------------------
 
 interface TaskInterFace {
   id: string;
   status: StatusesEnum;
 }
+
+// initially
+let tasks_1: TaskInterFace = {
+  id: "1234",
+  status: StatusesEnum.NOT_STARTED,
+};
+
+// on progress
+tasks_1.status = StatusesEnum.IN_PROGRESS;
+
+// upon completed
+tasks_1.status = StatusesEnum.DONE;
